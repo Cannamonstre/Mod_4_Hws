98cbab946ef0f8babe0117e1d6c5dda1b7e2044b
Dmitry <prrtaf@yandex.ru>
2024-08-09 23:09:22 +0300

    Initial

---
+++ Mod_4_1.py
@@ -0,0 +1,13 @@
+from Fake_Math import divide as fake_div
+
+from True_Math import divide as true_div
+
+result1 = fake_div(69, 3)
+result2 = fake_div(3, 0)
+result3 = true_div(49, 7)
+result4 = true_div(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
